0e0d342cdd52079d47a674d7a9af28064e24e278
shem090 <40526973+shem090@users.noreply.github.com>
2026-08-10 20:01:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,7 +115,7 @@ if df_base is not None:
     <div class="shock-card">
         <h3>⚙️ Задний амортизатор</h3>
         <div class="value-container">
-            <span class="value-label">🔹 Преднатяг пружины</span>
+            <span class="value-label">🔹 Преднатяг пружины (от полностью открученного)</span>
             <span class="value-badge-green">{b_z_pred} щелч.</span>
         </div>
         <div class="value-container">
