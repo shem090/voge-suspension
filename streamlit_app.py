e6e8f1e903c5314e35592d847d45236e22705ad6
shem090 <40526973+shem090@users.noreply.github.com>
2026-08-10 22:34:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -185,7 +185,7 @@ if df_base is not None:
                     except Exception:
                         st.error("Ошибка связи с сервером.")
                         
-                with nav_col3:
+                        with nav_col3:
             if st.button("Вперед ➡️", key=f"btn_next_{unique_suffix}", use_container_width=True):
                 st.session_state.review_index = (st.session_state.review_index + 1) % total_reviews
                 st.rerun()
